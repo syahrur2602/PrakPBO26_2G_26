0d8fa55a438f979293f5bcc9121d459bfa244b16
syahrur2602 <syahrur2602@gmail.com>
2026-09-11 09:10:06 +0700

    Mengubah method motor
    Added methods to start, stop, and increase speed of the motor.

--- Petemuan-3/motorEncapsulation/Motor.java
+++ Petemuan-3/motorEncapsulation/Motor.java
@@ -5,6 +5,23 @@ public class Motor {
     public int kecepatan = 0;
     public boolean kontakOn = false;
 
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+
+    public void matikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn == true) {
+            kecepatan += 5;
+        } else {
+            System.out.println("Kecepatan");
+        }
+    }
+
     public void printStatus() {
         if (kontakOn == true) {
             System.out.println("Kontak On");
