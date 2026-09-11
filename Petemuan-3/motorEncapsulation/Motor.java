7b18b4a8ba3fb7fcb01974ac2773efca03185020
syahrur2602 <syahrur2602@gmail.com>
2026-09-11 09:08:43 +0700

    Menambahkan file motor

---
+++ Petemuan-3/motorEncapsulation/Motor.java
@@ -0,0 +1,18 @@
+package Pertemuan_3.motorEncapsulation;
+
+public class Motor {
+
+    public int kecepatan = 0;
+    public boolean kontakOn = false;
+
+    public void printStatus() {
+        if (kontakOn == true) {
+            System.out.println("Kontak On");
+        } else {
+            System.out.println("Kontak Off");
+        }
+
+        System.out.println("Kecepatan " + kecepatan + "\n");
+    }
+    
+}
---
+++ Petemuan-3/motorEncapsulation/MotorDemo.java
@@ -0,0 +1,12 @@
+package Pertemuan_3.motorEncapsulation;
+
+public class MotorDemo {
+
+    public static void main(String[] args) {
+        Motor motor = new Motor();
+        motor.printStatus();
+        motor.kecepatan = 50;
+        motor.printStatus();
+    }
+    
+}
